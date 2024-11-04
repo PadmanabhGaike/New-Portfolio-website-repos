03e6ee2d49d513f71f01b02af18ae3f9afa739a8
unknown <Padmanabh Gaike>
2024-11-04 15:15:13 +0530

    updated formHandeler.js contact form logic

--- public/formHandeler.js
+++ public/formHandeler.js
@@ -33,3 +33,18 @@ document.getElementById('contact-form').addEventListener('submit', async functio
     }
   });
   
+
+  function openContactForm(){
+    const ContactForm= document.getElementById('contact-form');
+    const thankYouMessage= document.getElementById('thankYouMessage');
+
+    ContactForm.form.display='block';
+    thankYouMessage.style.display='none';
+
+
+    ContactForm.reset();
+
+    if (typeof grecaptcha !== 'undefined'){
+      grecaptcha.reset();
+    }
+  }
