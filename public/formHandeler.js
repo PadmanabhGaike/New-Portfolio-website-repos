3b4554a2c83966786d2cf991a104962995fb5f92
unknown <Padmanabh Gaike>
2024-11-06 20:40:58 +0530

    restored older version

--- public/formHandeler.js
+++ public/formHandeler.js
@@ -1,40 +1,39 @@
 document.getElementById('contact-form').addEventListener('submit', async function(event) {
-    event.preventDefault(); 
-  
-    
-    const recaptchaResponse = grecaptcha.getResponse();
-    if (!recaptchaResponse) {
-      alert("Please complete the reCAPTCHA.");
-      return;
-    }
-  
-    const formData = {
-      name: document.getElementById('name').value,
-      email: document.getElementById('email').value,
-      message: document.getElementById('message').value,
-      'g-recaptcha-response': recaptchaResponse 
-    };
-  
-    try {
-      const response = await fetch('/.netlify/functions/submit/submit', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(formData)
-      });
-  
-      if (response.ok) {
-        document.getElementById('contact-form').style.display = 'none';
-        document.getElementById('thankYouMessage').style.display = 'block';
-        
-        document.getElementById('contactForm').reset();
+  event.preventDefault(); 
 
-        grecaptcha.reset();
-
-      } else {
-        alert('There was an issue submitting your message. Please try again later.');
-      }
-    } catch (error) {
-      console.error('Error submitting form:', error);
-    }
-  });
   
+  const recaptchaResponse = grecaptcha.getResponse();
+  if (!recaptchaResponse) {
+    alert("Please complete the reCAPTCHA.");
+    return;
+  }
+
+  const formData = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    message: document.getElementById('message').value,
+    'g-recaptcha-response': recaptchaResponse 
+  };
+
+  try {
+    const response = await fetch('/.netlify/functions/submit/submit', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    });
+
+    if (response.ok) {
+      document.getElementById('contact-form').style.display = 'none';
+      document.getElementById('thankYouMessage').style.display = 'block';
+
+      document.getElementById('contactForm').reset();
+
+      grecaptcha.reset();
+
+    } else {
+      alert('There was an issue submitting your message. Please try again later.');
+    }
+  } catch (error) {
+    console.error('Error submitting form:', error);
+  }
+});
